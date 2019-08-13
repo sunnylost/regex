6dbc5bed157a9914cddd58b794d7e2185656d9a8
sunnylost <sunnylost@gmail.com>
2019-08-13 23:08:06 +0800

    update assert unit test

--- test/assertion.ts
+++ test/assertion.ts
@@ -86,3 +86,107 @@ test('15.10.2.6_A2_T2', () => {
     expect(result.index).toBe(expected.index)
     expect(result).toEqual(expected)
 })
+
+test('15.10.2.6_A2_T3', () => {
+    let re = new Re('^p[a-z]')
+    let source = 'pairs\nmakes\tdouble\npesos'
+    let result = re.match(source)
+
+    let expected: IMatchResult = ['pa']
+    expected.index = 0
+    expected.input = source
+
+    expect(result.length).toBe(expected.length)
+    expect(result.index).toBe(expected.index)
+    expect(result).toEqual(expected)
+})
+
+test('15.10.2.6_A2_T4', () => {
+    let re = new Re('^p[b-z]', 'm')
+    let source = 'pairs\nmakes\tdouble\npesos'
+    let result = re.match(source)
+
+    let expected: IMatchResult = ['pe']
+    expected.index = 19
+    expected.input = source
+
+    expect(result.length).toBe(expected.length)
+    expect(result.index).toBe(expected.index)
+    expect(result).toEqual(expected)
+})
+
+test('15.10.2.6_A2_T5', () => {
+    let re = new Re('^[^p]', 'm')
+    let source = 'pairs\nmakes\tdouble\npesos'
+    let result = re.match(source)
+
+    let expected: IMatchResult = ['m']
+    expected.index = 6
+    expected.input = source
+
+    expect(result.length).toBe(expected.length)
+    expect(result.index).toBe(expected.index)
+    expect(result).toEqual(expected)
+})
+
+test('15.10.2.6_A2_T6', () => {
+    let re = new Re('^ab')
+    let source = 'abcde'
+    let result = re.match(source)
+
+    let expected: IMatchResult = ['ab']
+    expected.index = 0
+    expected.input = source
+
+    expect(result.length).toBe(expected.length)
+    expect(result.index).toBe(expected.index)
+    expect(result).toEqual(expected)
+})
+
+test('15.10.2.6_A2_T7', () => {
+    let re = new Re('^..^e')
+    let source = 'ab\ncde'
+    let result = re.match(source)
+
+    let expected: IMatchResult = null
+
+    expect(result).toEqual(expected)
+})
+
+test('15.10.2.6_A2_T8', () => {
+    let re = new Re('^xxx')
+    let source = 'yyyyy'
+    let result = re.match(source)
+
+    let expected: IMatchResult = null
+
+    expect(result).toEqual(expected)
+})
+
+test('15.10.2.6_A2_T9', () => {
+    let re = new Re('^\\^+')
+    let source = '^^^x'
+    let result = re.match(source)
+
+    let expected: IMatchResult = ['^^^']
+    expected.index = 0
+    expected.input = source
+
+    expect(result.length).toBe(expected.length)
+    expect(result.index).toBe(expected.index)
+    expect(result).toEqual(expected)
+})
+
+test('15.10.2.6_A2_T10', () => {
+    let re = new Re('^\\d+', 'm')
+    let source = 'abc\n123xyz'
+    let result = re.match(source)
+
+    let expected: IMatchResult = ['123']
+    expected.index = 4
+    expected.input = source
+
+    expect(result.length).toBe(expected.length)
+    expect(result.index).toBe(expected.index)
+    expect(result).toEqual(expected)
+})
